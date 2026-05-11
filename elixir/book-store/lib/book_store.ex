627d7c03a2baceb5878e0e35ec6926cccf529358
Jamie Curle <jamie@curle.io>
2026-05-11 17:20:32 +0100

    solved: bookstore now has more comments for future me

--- elixir/book-store/lib/book_store.ex
+++ elixir/book-store/lib/book_store.ex
@@ -15,18 +15,29 @@ defmodule BookStore do
       distribute(item, groups)
     end)
     # convert to a map of frequencies, we're looking for %{5 => 1, 3 => 1}
+    # we don't need to know "what" is in the basket, so we can safely convert
+    # to a data structure that's easier to work with such
+    # # as "three groups twice"
+    # %{3 => 2}
+    # or more importantly, 5 /3  groups
     |> Enum.frequencies_by(&length/1)
-    # fix the 5/3 groups
+    # fix the 5/3 groups ... %{5 => 1, 3 => 1}
     |> rebalance()
-    #  and now we
-    |> Enum.reduce(0, fn {size, count}, acc -> acc + count * size * price(size) end)
+    #  finally we can calculate the price and that's easier now becasue
+    #  reduce on a map gives us key, values which translates nicely into
+    #  qty and how many groups of that quantity we have.
+    |> Enum.reduce(0, fn {qty, count}, acc -> acc + count * qty * price(qty) end)
+
+    # I'm going to work up to the hard ones from now on...
   end
 
   defp rebalance(counts) do
-    # what pairs can we take
+    # how many pairs of 5 / 3 groups can we take?
     pairs = min(Map.get(counts, 5, 0), Map.get(counts, 3, 0))
 
     counts
+    # subtract from five and three groups, and insert the more
+    # optimal four groups.
     |> Map.update(5, 0, &(&1 - pairs))
     |> Map.update(3, 0, &(&1 - pairs))
     |> Map.update(4, pairs * 2, &(&1 + pairs * 2))
